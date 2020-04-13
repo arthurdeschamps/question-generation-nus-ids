Add possibility to load google BERT models. Fix bug where gradient was computed for padding tokens.

data_utils/embeddings.py
@@ -1,7 +1,6 @@
 from typing import List
 import numpy as np
 import tensorflow as tf
-from transformers import BertTokenizer
 from data_utils.class_defs import SquadExample, Question, Answer
 
 
@@ -11,10 +10,10 @@ class Embedder:
     """
     HL_TOKEN = '[unused1]'  # This token is used to indicate where the answer starts and finishes
 
-    def __init__(self, pretrained_model_name):
+    def __init__(self, pretrained_model_name, tokenizer):
         super(Embedder, self).__init__()
         self.pretrained_weights_name = pretrained_model_name
-        self.tokenizer = BertTokenizer.from_pretrained(self.pretrained_weights_name)
+        self.tokenizer = tokenizer
 
         self.padding_token = tf.constant(self.tokenizer.pad_token_id, dtype=tf.int32)
         self.mask_token = tf.constant(self.tokenizer.mask_token_id, dtype=tf.int32)
@@ -110,7 +109,7 @@ class Embedder:
                             input_emb = self.generate_bert_hlsqg_input_embedding(paragraph.context, answer)
                             label_emb = self.generate_bert_hlsqg_output_embedding(question)
                             # Maximum sequence length of this model
-                            if len(input_emb) <= max_sequence_length - max_generated_question_length:
+                            if len(input_emb) < max_sequence_length - max_generated_question_length:
                                 x.append(np.array(input_emb, dtype=np.int32))
                                 y.append(np.array(label_emb, dtype=np.int32))
         return x, y

models/bert.py
@@ -1,5 +1,4 @@
 import numpy as np
-from transformers import TFBertModel as BertModel
 from data_utils.embeddings import Embedder
 import tensorflow as tf
 from models.base_model import BaseModel
@@ -10,7 +9,9 @@ class Bert(BaseModel):
     BERT implementation of this fine-tuning architecture: https://www.aclweb.org/anthology/D19-5821.pdf.
     """
 
-    def __init__(self, embedder: Embedder,
+    def __init__(self,
+                 embedder: Embedder,
+                 model,
                  max_generated_question_length=20,
                  beam_search_size=3,
                  max_sequence_length=512,
@@ -27,7 +28,7 @@ class Bert(BaseModel):
         self.beam_search_size = beam_search_size
         self.embedder = embedder
         self.pretrained_weights_name = embedder.pretrained_weights_name
-        self.model = BertModel.from_pretrained(self.pretrained_weights_name)
+        self.model = model
         self.max_sequence_length = max_sequence_length
 
         initializer = tf.initializers.glorot_uniform()

training/train.py
@@ -1,9 +1,12 @@
 import tensorflow as tf
+from transformers import BertTokenizer, TFBertModel, BertConfig
 from data_utils.embeddings import Embedder
+from data_utils.parse import read_bert_config
 from models.bert import Bert
 from training.trainer import Trainer
 from training.utils.model_manager import ModelManager
 from training.utils.squad_dataset import SquadDataset
+from defs import PRETRAINED_MODELS_DIR
 
 flags = tf.compat.v1.flags
 
@@ -12,7 +15,7 @@ flags.DEFINE_boolean('train', True, 'If training must be performed or only evalu
 flags.DEFINE_integer('nb_epochs', 5, 'Number of epochs.')
 flags.DEFINE_integer('batch_size', 1, 'Batch size.  Must divide evenly into the dataset sizes.')
 flags.DEFINE_boolean('debug', False, 'If to run in debug mode or not.')
-flags.DEFINE_string('pretrained_model_name', 'bert-base-uncased', 'Name of the pre-trained BERT model.')
+flags.DEFINE_string('pretrained_model_name', 'bert_mini_uncased', 'Name of the pre-trained BERT model.')
 flags.DEFINE_boolean('load_model', False, 'If the model should be trained or loaded from memory for evaluation')
 flags.DEFINE_string('loaded_model_name', None, 'Name of the model to load.')
 flags.DEFINE_boolean('save_model', True, 'If the model is to be saved or not (after training).')
@@ -27,8 +30,24 @@ training = FLAGS.train
 for flag, flag_val in FLAGS.flag_values_dict().items():
     print(f"{flag}: {flag_val}")
 
-embedder = Embedder(FLAGS.pretrained_model_name)
-model = Bert(embedder)
+if FLAGS.pretrained_model_name == "bert_base_uncased":
+    pretrained_model_name = "bert-base-uncased"
+    vocab_name = pretrained_model_name
+    base_model = TFBertModel.from_pretrained(pretrained_model_name)
+    bert_config = base_model.config
+elif FLAGS.pretrained_model_name == "bert_mini_uncased":
+    pretrained_model_name = "uncased_L-4_H-256_A-4"
+    vocab_name = f"{PRETRAINED_MODELS_DIR}/{pretrained_model_name}/"
+    bert_config = read_bert_config(pretrained_model_name)
+    base_model = TFBertModel(bert_config)
+    base_model = ModelManager.load_pretrained_model(base_model, f"{pretrained_model_name}/bert_model.ckpt.index")
+else:
+    raise NotImplementedError()
+
+tokenizer = BertTokenizer.from_pretrained(vocab_name)
+embedder = Embedder(pretrained_model_name, tokenizer)
+model = Bert(embedder, model=base_model,  hidden_state_size=bert_config.hidden_size)
+
 if FLAGS.load_model:
     if FLAGS.loaded_model_name is None:
         raise ValueError("loaded_model_name was None while load_model was requested")
@@ -42,11 +61,11 @@ ds = SquadDataset(max_sequence_length=model.max_sequence_length,
                   limit_dev_data=FLAGS.limit_dev_data,
                   limit_train_data=FLAGS.limit_train_data)
 if training:
-    train_ds = ds.get_train_set() if not debug else ds.get_dev_set()
-    dev_ds = ds.get_dev_set() if not debug else train_ds
+    train_ds = ds.get_train_set()
+    dev_ds = ds.get_dev_set()
 else:
     train_ds = None
-    dev_ds = ds.get_dev_set() if not debug else ds.get_dev_set()
+    dev_ds = ds.get_dev_set()
 
 
 trainer = Trainer(model)
@@ -68,6 +87,9 @@ for epoch in range(EPOCHS):
 
     for test_features, test_labels in dev_ds:
         trainer.test_step(test_features, tf.squeeze(test_labels))
+
+    #  logdir = "logs/scalars/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+    #  tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
 
     #  template = 'Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'
     template = 'Epoch {}, Train Loss: {}, Mean Accuracy: {}, Test loss: {}'

training/trainer.py
@@ -44,7 +44,6 @@ class Trainer:
                 loss = self.train_loss_object(padding_free_outputs, padding_free_distributions)
 
             def compute_and_apply_gradients():
-                tf.print("Computing gradient - Token number ", i)
                 gradients = tape.gradient(loss, self.model.trainable_variables)
                 self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
                 self.train_loss(loss)
@@ -52,7 +51,7 @@ class Trainer:
 
             # Only backpropagates for non padding outputs
             tf.cond(
-                tf.greater(padding_free_outputs.shape[1], tf.constant(0)),
+                tf.greater(tf.size(padding_free_outputs), tf.constant(0)),
                 compute_and_apply_gradients,
                 lambda: None
             )

training/utils/model_manager.py
@@ -1,6 +1,6 @@
 import time
 import tensorflow as tf
-from defs import TRAINED_MODELS_DIR
+from defs import TRAINED_MODELS_DIR, PRETRAINED_MODELS_DIR
 
 
 class ModelManager:
@@ -17,6 +17,11 @@ class ModelManager:
     @staticmethod
     def load_model(model: tf.keras.Model, model_name):
         model.load_weights(ModelManager._model_filepath(model_name))
+        return model
+
+    @staticmethod
+    def load_pretrained_model(model, model_name):
+        model.load_weights(f"{PRETRAINED_MODELS_DIR}/{model_name}")
         return model
 
     @staticmethod