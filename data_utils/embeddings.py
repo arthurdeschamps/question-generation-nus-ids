Add tensorboard logging. Correct multiple incorrect computations during training.

data_utils/embeddings.py
@@ -81,14 +81,18 @@ class Embedder:
         return self.tokenizer.encode(tokens, add_special_tokens=False)
 
     def generate_bert_hlsqg_output_embedding(self, question: Question):
-        # TODO: I don't think the outputs need to be encoded
-        return self.tokenizer.encode(self.tokenizer.tokenize(question.question), add_special_tokens=False)
+        output_tokens = self.tokenizer.encode(self.tokenizer.tokenize(question.question), add_special_tokens=False)
+        output_tokens.append(self.tokenizer.sep_token_id)
+        return output_tokens
 
-    def generate_bert_hlsqg_dataset(self, squad_examples: List[SquadExample],
+    def generate_bert_hlsqg_dataset(self,
+                                    squad_examples: List[SquadExample],
                                     max_sequence_length,
-                                    max_generated_question_length):
+                                    max_generated_question_length,
+                                    limit=-1):
         """
         Generates a dataset for the HLSQG Bert architecture using the given SQuAD examples.
+        :param limit: Number of datapoints to generate (-1 means no limit).
         :param squad_examples: A list of squad example objects.
         :param max_sequence_length: The maximum sequence length supported by the model to be used with this dataset.
         :param max_generated_question_length: The generated questions maximum length.
@@ -96,14 +100,15 @@ class Embedder:
         """
         x = []
         y = []
+        generated = 0
         for squad_example in squad_examples:
             for paragraph in squad_example.paragraphs:
                 for qa in paragraph.qas:
                     # [CLS], c_1, c_2, ..., [HL] a_1, ..., a_|A|m [HL], ..., c_|C|, [SEP], [MASK]
                     # Where C is the context, A the answer (within the context, marked by special
                     # characters [HL] ... [HL]).
-                    question = qa[0]
-                    answers = qa[1]
+                    question = qa.question
+                    answers = qa.answers
                     if len(answers) > 0:  # Makes sure the question in answerable
                         for answer in answers:
                             input_emb = self.generate_bert_hlsqg_input_embedding(paragraph.context, answer)
@@ -112,6 +117,9 @@ class Embedder:
                             if len(input_emb) < max_sequence_length - max_generated_question_length:
                                 x.append(np.array(input_emb, dtype=np.int32))
                                 y.append(np.array(label_emb, dtype=np.int32))
+                                generated += 1
+                                if (limit > -1) and (generated >= limit):
+                                    return x, y
         return x, y
 
     def vocab_size(self):

data_utils/squad_dataset.py
@@ -1,3 +1,6 @@
+from typing import List
+
+from data_utils.class_defs import SquadExample
 from data_utils.parse import read_squad_dataset
 from data_utils.pre_processing import pad_data
 import tensorflow as tf
@@ -28,18 +31,21 @@ class SquadDataset:
         self.limit_dev_data = limit_dev_data
 
     def get_train_set(self):
-        ds = self._prepare_data(read_squad_dataset(SQUAD_TRAIN, limit=self.limit_train_data),
-                                limit=self.limit_train_data)
-        return ds.shuffle(buffer_size=256, reshuffle_each_iteration=True).repeat(self.epochs).batch(self.batch_size)
+        ds, ds_size = self._prepare_data(read_squad_dataset(SQUAD_TRAIN, limit=self.limit_train_data),
+                                         limit=self.limit_train_data)
+        return ds.shuffle(buffer_size=ds_size, reshuffle_each_iteration=True).repeat(self.epochs).batch(self.batch_size)
 
     def get_dev_set(self):
         return self._prepare_data(read_squad_dataset(SQUAD_DEV, limit=self.limit_dev_data),
-                                  limit=self.limit_dev_data).batch(1).repeat(self.epochs)
+                                  limit=self.limit_dev_data)[0].batch(1).repeat(self.epochs)
 
-    def _prepare_data(self, data, limit):
-        x, y = self.embedder.generate_bert_hlsqg_dataset(
-            data, self.max_sequence_length, self.max_generated_question_length)
+    def _prepare_data(self, data: List[SquadExample], limit):
+        contexts, questions = self.embedder.generate_bert_hlsqg_dataset(
+            data,
+            self.max_sequence_length, self.max_generated_question_length,
+            limit=limit
+        )
         padding_value = self.embedder.tokenizer.pad_token_id
-        x = pad_data(x, padding_value)[:limit]
-        y = pad_data(y, padding_value)[:limit]
-        return tf.data.Dataset.from_tensor_slices((x, y))
+        contexts = pad_data(contexts, padding_value)
+        questions = pad_data(questions, padding_value)
+        return tf.data.Dataset.from_tensor_slices((contexts, questions)), len(contexts)

models/bert.py
@@ -96,7 +96,7 @@ class Bert(BaseModel):
             indices=tf.where(tf.not_equal(generated_question, self.embedder.mask_token))
         ), shape=(-1,))
 
-    def step(self, tokens, correct_output_tokens, step=tf.Variable(0, dtype=tf.int32)):
+    def step(self, tokens, correct_output_tokens, step=tf.Variable(0, dtype=tf.int32, name='sequence_pointer')):
         """
         To use for teacher forcing training.
         :param tokens: A token sequence representing a context. Shape should be (batch_size, token_sequences_length).
@@ -105,18 +105,20 @@ class Bert(BaseModel):
         :return: Distributions for the next words, the correct next outputs and new input tokens to use for the next
         iteration.
         """
-
         correct_next_outputs = correct_output_tokens[:, step]
-        hidden_states = self.model(tokens)[0]
-        mask_states = hidden_states[:, -1, :]
-        word_distributions = tf.math.softmax(tf.add(tf.matmul(mask_states, self.W_sqg), self.b_sqg))
+        attention_mask = tf.cast(tf.not_equal(tokens, self.embedder.padding_token), dtype=tf.int32,
+                                 name='attention_mask')
+        hidden_states = self.model(tokens, attention_mask=attention_mask)[0]
+        mask_indices = tf.reshape(tf.reduce_sum(attention_mask, axis=1) - 1, shape=(hidden_states.shape[0], 1))
+        mask_states = tf.gather_nd(hidden_states, mask_indices, batch_dims=1)
+        word_logits = tf.add(tf.matmul(mask_states, self.W_sqg), self.b_sqg, name='word_logits')
         # Uses the correct next token (teacher forcing)
         new_input_tokens = self.embedder.generate_next_input_tokens(
             tokens,
             correct_next_outputs,
             padding_token=self.embedder.padding_token
         )
-        return word_distributions, correct_next_outputs, new_input_tokens
+        return word_logits, correct_next_outputs, new_input_tokens
 
     def _compute_next_beams(self, ite, current_beams, current_beam_probs):
         """
@@ -135,7 +137,7 @@ class Bert(BaseModel):
                 tf.unstack(self.embedder.generate_next_input_tokens(current_beams,
                                                                     top_preds_indices[j],
                                                                     padding_token=self.embedder.padding_token)))
-            new_beams_probs.extend(tf.unstack(tf.multiply(current_beam_probs, top_preds_probs[j])))
+            new_beams_probs.extend(tf.unstack(tf.multiply(current_beam_probs[j], top_preds_probs[j])))
         # Only keeps the k most probably sequences (out of k^2).
         current_beam_probs, top_beams_indices = tf.math.top_k(new_beams_probs, k=self.beam_search_size)
         current_beams = tf.gather(new_beams, top_beams_indices)

training/train.py
@@ -1,11 +1,13 @@
+import datetime
+
 import tensorflow as tf
-from transformers import BertTokenizer, TFBertModel, BertConfig
+from transformers import BertTokenizer, TFBertModel
 from data_utils.embeddings import Embedder
 from data_utils.parse import read_bert_config
 from models.bert import Bert
 from training.trainer import Trainer
 from training.utils.model_manager import ModelManager
-from training.utils.squad_dataset import SquadDataset
+from data_utils.squad_dataset import SquadDataset
 from defs import PRETRAINED_MODELS_DIR
 
 flags = tf.compat.v1.flags
@@ -19,9 +21,10 @@ flags.DEFINE_string('pretrained_model_name', 'bert_mini_uncased', 'Name of the p
 flags.DEFINE_boolean('load_model', False, 'If the model should be trained or loaded from memory for evaluation')
 flags.DEFINE_string('loaded_model_name', None, 'Name of the model to load.')
 flags.DEFINE_boolean('save_model', True, 'If the model is to be saved or not (after training).')
-flags.DEFINE_string('saved_model_name', None, 'Name of the model to save.')
+flags.DEFINE_string('saved_model_name', datetime.datetime.now().strftime("%Y%m%d-%H%M%S"), 'Name of the model to save.')
 flags.DEFINE_integer('limit_train_data', -1, 'Number of rows to take from the training set.')
 flags.DEFINE_integer('limit_dev_data', -1, 'Number of rows to take from the dev set.')
+flags.DEFINE_boolean('log_test_metrics', True, 'Whether to log metrics for tensorboard at test time.')
 
 EPOCHS = FLAGS.nb_epochs
 debug = FLAGS.debug
@@ -68,8 +71,11 @@ else:
     dev_ds = ds.get_dev_set()
 
 
-trainer = Trainer(model)
-step = tf.Variable(0, dtype=tf.int32, name='step')
+trainer = Trainer(model, model_name=FLAGS.saved_model_name)
+# This is the position of the pointer within sentences
+step = tf.Variable(0, dtype=tf.int32, name='step', trainable=False)
+# This is how many times backprob has been performed
+global_step = tf.Variable(0, dtype=tf.int64, name='global_step', trainable=False)
 
 for epoch in range(EPOCHS):
     # Reset the metrics at the start of the next epoch
@@ -82,14 +88,11 @@ for epoch in range(EPOCHS):
         for features, labels in train_ds:
             prev_time = tf.timestamp()
             step.assign(0)
-            trainer.train_step(features, labels, step)
+            trainer.train_step(features, labels, step, global_step)
             tf.print("Step completed in ", (tf.timestamp() - prev_time), " seconds")
 
     for test_features, test_labels in dev_ds:
-        trainer.test_step(test_features, tf.squeeze(test_labels))
-
-    #  logdir = "logs/scalars/" + datetime.now().strftime("%Y%m%d-%H%M%S")
-    #  tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
+        trainer.test_step(test_features, tf.squeeze(test_labels), global_step, FLAGS.log_test_metrics)
 
     #  template = 'Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'
     template = 'Epoch {}, Train Loss: {}, Mean Accuracy: {}, Test loss: {}'

training/trainer.py
@@ -1,5 +1,8 @@
-import tensorflow as tf
+import os
+import shutil
 
+import tensorflow as tf
+from defs import GRADIENT_DIR
 from training.utils.rouge_score import rouge_n
 
 
@@ -8,8 +11,8 @@ class Trainer:
     Use to train a model.
     """
 
-    def __init__(self, model,
-                 train_loss_object=tf.losses.SparseCategoricalCrossentropy(from_logits=False),
+    def __init__(self, model, model_name,
+                 train_loss_object=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
                  optimizer=tf.optimizers.Adam(),
                  train_loss=tf.keras.metrics.Mean(name='train_loss'),
                  train_accuracy=tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy'),
@@ -25,29 +28,43 @@ class Trainer:
         self.test_loss = test_loss
         self.test_accuracy = test_accuracy
 
+        model_gradient_dir = f"{GRADIENT_DIR}/{model_name}"
+        if os.path.isdir(model_gradient_dir):
+            shutil.rmtree(model_gradient_dir)
+        self.train_summary_writer = tf.summary.create_file_writer(f"{model_gradient_dir}/train")
+        self.test_summary_writer = tf.summary.create_file_writer(f"{model_gradient_dir}/test")
+
     @tf.function
-    def train_step(self, paragraph_tokens_batches, target_question_tokens_batches, step):
+    def train_step(self, paragraph_tokens_batches, target_question_tokens_batches, step, global_step: tf.Variable):
+
         def compute_loss(i, answer_tokens, question_tokens):
             # Only computes the loss for non-padding tokens (that is, tokens that are actually part of the question)
             with tf.GradientTape() as tape:
-                word_distributions, correct_outputs, new_input_tokens = self.model.step(answer_tokens, question_tokens,
-                                                                                        i)
+                word_logits, correct_outputs, new_input_tokens = self.model.step(answer_tokens, question_tokens, i)
                 padding_free_indices = tf.where(tf.not_equal(correct_outputs,
                                                              tf.fill(
                                                                  correct_outputs.shape,
                                                                  value=tf.constant(self.embedder.tokenizer.pad_token_id,
                                                                                    tf.int32)
                                                              )))
-                padding_free_distributions = tf.gather(word_distributions, padding_free_indices)
-                padding_free_predictions = tf.math.softmax(padding_free_distributions, axis=1)
+                padding_free_logits = tf.gather(word_logits, padding_free_indices)
+                padding_free_predictions = tf.math.argmax(tf.math.softmax(padding_free_logits, axis=-1), axis=-1)
                 padding_free_outputs = tf.gather(correct_outputs, padding_free_indices)
-                loss = self.train_loss_object(padding_free_outputs, padding_free_distributions)
+
+                # Computes the perplexity
+                loss = self.train_loss_object(padding_free_outputs, padding_free_logits)
 
             def compute_and_apply_gradients():
+                global_step.assign(global_step + 1)
                 gradients = tape.gradient(loss, self.model.trainable_variables)
                 self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
                 self.train_loss(loss)
                 self.train_accuracy(padding_free_outputs, padding_free_predictions)
+                tf.print(tf.map_fn(lambda preds: tf.py_function(
+                    self.embedder.tokenizer.convert_ids_to_tokens,
+                    inp=[preds],
+                    Tout=tf.string
+                ), padding_free_predictions, dtype=tf.string))
 
             # Only backpropagates for non padding outputs
             tf.cond(
@@ -75,9 +92,13 @@ class Trainer:
                               tf.TensorShape([paragraph_tokens_batches.shape[0], None]),
                               target_question_tokens_batches.shape]
         )
+        with self.train_summary_writer.as_default():
+            tf.print("Sentence loss: ", self.train_loss.result())
+            tf.summary.scalar('loss', self.train_loss.result(), step=global_step)
+            self.train_loss.reset_states()
 
     @tf.function
-    def test_step(self, paragraph_tokens, target_question_tokens):
+    def test_step(self, paragraph_tokens, target_question_tokens, global_step, log_metrics):
         # training=False is only needed if there are layers with different
         # behavior during training versus inference (e.g. Dropout).
         predictions = self.model(paragraph_tokens, training=False)
@@ -85,11 +106,14 @@ class Trainer:
             tf.not_equal(target_question_tokens, self.embedder.padding_token))), shape=(-1,))
 
         def compute_accuracy(target_question, generated_question):
-            target_question = self.embedder.vocab_lookup(target_question.numpy()).replace('?', '')
+            target_question = self.embedder.vocab_lookup(target_question.numpy())
             generated_question = self.embedder.vocab_lookup(generated_question.numpy()).replace('?', '')
-            print("\n" + target_question)
-            print(generated_question)
-            return self.test_accuracy([generated_question], [target_question])
+            tf.print(generated_question)
+            acc = self.test_accuracy([generated_question], [target_question])
+            if log_metrics:
+                with self.test_summary_writer.as_default():
+                    tf.summary.scalar('dev_accuracy', acc, step=global_step)
+            return acc
 
         self.test_loss(tf.py_function(
             compute_accuracy,