Fix pad tokens masking for logits. Add some of the analyses files from the thesis.

data_processing/repeat_q_dataset.py
@@ -68,8 +68,8 @@ class RepeatQDataset:
             base_question = self.words_to_ids(example.base_question.split())
             base_questions.append(base_question)
             base_questions_features.append(self.features_to_ids(example.base_question_features))
-            facts = [self.words_to_ids(fact.split(' ')) for fact in example.facts]
-            facts_features.append([self.features_to_ids(fact_features) for fact_features in example.facts_features])
+            facts = [self.words_to_ids(fact.split(' ')) for fact in example.facts][:3]
+            facts_features.append([self.features_to_ids(fact_features) for fact_features in example.facts_features][:3])
             max_fact_length = max(max_fact_length, max(len(fact) for fact in facts))
             max_nb_facts = max(max_nb_facts, len(facts))
             facts_list.append(facts)
@@ -78,7 +78,7 @@ class RepeatQDataset:
         base_questions = self.sequence_padding(base_questions)
         targets = self.sequence_padding(targets)
         is_from_base_question = self.sequence_padding(is_from_base_question)
-        facts_list = np.array(self.matrix_padding(facts_list, max_length=max_fact_length, max_width=max_nb_facts))
+        facts_list = np.array(self.matrix_padding(facts_list, max_length=32, max_width=max_nb_facts))
 
         for k in range(len(self.ds)):
             base_question = base_questions[k]

evaluating/model_benchmark.py
@@ -1,4 +1,5 @@
 import json
+import sys
 
 from evaluating.rouge_score import rouge_l_sentence_level as rouge_l
 import nltk.translate.bleu_score as bleu
@@ -9,9 +10,13 @@ import numpy as np
 from defs import NQG_MEDQUAD_DATASET, NQG_MEDQUAD_PREDS_OUTPUT_PATH, NQG_SQUAD_PREDS_OUTPUT_PATH, \
     NQG_SQUAD_GA_PREDS_OUTPUT_PATH, NQG_SQUAD_NA_PREDS_OUTPUT_PATH, NQG_SQUAD_NER_PREDS_OUTPUT_PATH, \
     NQG_SQUAD_DATASET, NQG_SQUAD_NER_DATASET, NQG_SQUAD_TESTGA_PREDS_OUTPUT_PATH, SG_DQG_HOTPOT_PREDS_PATH, \
-    HOTPOT_QA_DEV_TARGETS_PATH, ASS2S_SQUAD_PREDS_OUTPUT_PATH, ASS2S_PROVIDED_PROCESSED_DATA_DIR, \
-    REPEAT_Q_SQUAD_DATA_DIR, REPEAT_Q_SQUAD_OUTPUT_FILEPATH, ASS2S_PROCESSED_SQUAD_DIR, SG_DQG_SQUAD_PREDS_PATH
+    REPEAT_Q_SQUAD_DATA_DIR, REPEAT_Q_SQUAD_OUTPUT_FILEPATH, ASS2S_PROCESSED_SQUAD_DIR, SG_DQG_SQUAD_PREDS_PATH, \
+    REPEAT_Q_PREDS_OUTPUT_DIR, ASS2S_PRED_DIR, ASS2S_DIR, ROOT_DIR
+
+sys.path.append(ASS2S_DIR + '/submodule/')
+sys.path.append(f"{ROOT_DIR}/evaluating/coco-caption")
 from mytools import remove_adjacent_duplicate_grams
+from pycocoevalcap.meteor.meteor import Meteor
 
 
 def corpus_f1_score(corpus_candidates, corpus_references):
@@ -41,7 +46,11 @@ def benchmark(corpus_candidates: np.ndarray, corpus_references: np.ndarray):
     print(f"ROUGE-L: {rouge_l_sentence_level}")
     meteor_score = 100 * np.mean(np.array([meteor(references, candidate)
                                      for (references, candidate) in zip(corpus_references, corpus_candidates)]))
-    print(f"METEOR macro average: {meteor_score}")
+    # meteor_score = Meteor().compute_score(
+    #     gts={i: corpus_references[i] for i in range(len(corpus_references))},
+    #     res={i: [corpus_candidates[i]] for i in range(len(corpus_candidates))}
+    # )
+    print(f"METEOR: {meteor_score}")
     f1_score = 100 * corpus_f1_score(corpus_candidates_split, corpus_references_split)
     print(f"F1 macro average: {f1_score}")
 
@@ -88,7 +97,8 @@ if __name__ == '__main__':
 
     models = (
         "nqg_squad", "nqg_squad_ga", "nqg_squad_na", "nqg_squad_ner", "nqg_medquad", "nqg_squad_testga",
-        "sg_dqg_hotpotqa", "ass2s_squad", "sg_dqg_squad", "repeat_q_squad"
+        "sg_dqg_hotpotqa", "ass2s_squad", "sg_dqg_squad", "repeat_q_squad", "repeat_q_squad_mapped_triples",
+        "thesis_bad_example", "repeat_q_squad_mturk", "ass2s_squad_repeat_q_mturk", "ass2s_squad_repeat_q"
     )
 
     import argparse
@@ -153,37 +163,51 @@ if __name__ == '__main__':
             candidates, references = get_sg_dqg_predictions(SG_DQG_SQUAD_PREDS_PATH)
 
     elif "ass2s" in model:
+        candidate_file = ASS2S_PRED_DIR
+        reference_dir = ASS2S_PROCESSED_SQUAD_DIR
         if model == "ass2s_squad":
-            candidates = np.array(pd.read_csv(ASS2S_SQUAD_PREDS_OUTPUT_PATH, header=None, sep='\n')).reshape((-1,))
-            references = np.array(
-                pd.read_csv(f"{ASS2S_PROCESSED_SQUAD_DIR}/filtered_txt/test_question_origin.txt", header=None, sep='\n')
-            ).reshape((-1, 1))
-            assert candidates.shape[0] == references.shape[0]
+            candidate_file += "/squad_original_preds.txt"
+        elif model == "ass2s_squad_repeat_q_mturk":
+            candidate_file += "/squad_mturk_preds.txt"
+            reference_dir = reference_dir.replace("squad", "squad_mturk")
+        elif model == "ass2s_squad_repeat_q":
+            candidate_file += "/squad_preds.txt"
+        else:
+            raise ValueError(f"Model {model} not recognized.")
+
+        candidates = np.array(pd.read_csv(candidate_file, header=None, sep='\n')).reshape((-1,))
+        references = np.array(
+            pd.read_csv(f"{reference_dir}/filtered_txt/test_question_origin.txt", header=None, sep='\n')
+        ).reshape((-1, 1))
+        assert candidates.shape[0] == references.shape[0]
 
     elif "repeat_q" in model:
         if model == "repeat_q_squad":
-            with open(f"{REPEAT_Q_SQUAD_DATA_DIR}/test.data.json", mode='r') as test_file:
-                test_data = json.load(test_file)
-            references = {}
-            candidates = {}
+            target_dir = REPEAT_Q_SQUAD_DATA_DIR
+            prediction_file = f"{REPEAT_Q_PREDS_OUTPUT_DIR}/full_dataset_sentence_facts_predictions.txt"
+        elif model == "repeat_q_squad_mapped_triples":
+            target_dir = f"{REPEAT_Q_SQUAD_DATA_DIR}_mapped_triples"
+            prediction_file = f"{REPEAT_Q_PREDS_OUTPUT_DIR}/mixed_data_predictions.txt"
+        elif model == "repeat_q_squad_mturk":
+            target_dir = REPEAT_Q_SQUAD_DATA_DIR
+            prediction_file = f"{REPEAT_Q_PREDS_OUTPUT_DIR}/mturk_attempts_predictions.txt"
+        else:
+            raise NotImplementedError(f"'{model}' not recognized")
 
-            for data in test_data:
-                q = data["base_question"]
-                # Only keeps organic data for evaluation
-                if data["passage_id"] == -1:
-                    rewrites = references.get(q)
-                    if rewrites is None:
-                        rewrites = [data["target"]]
-                    else:
-                        rewrites.append(data["target"])
-                    references[q] = rewrites
+        with open(f"{target_dir}/test.data.json", mode='r') as test_file:
+            test_data = json.load(test_file)
 
-            base_questions = [k for k, _ in references.items()]
-            references = [v for _, v in references.items()]
-            candidates = np.array(pd.read_csv(
-                REPEAT_Q_SQUAD_OUTPUT_FILEPATH, header=None, sep='\n', comment=None)
-            ).reshape((-1,))
-            # Keeps insertion order and gets rid of duplicates
-            candidates = list({c: None for c in candidates}.keys())
+        references = [[d["target"]] for d in test_data]
+        candidates = np.array(pd.read_csv(prediction_file, header=None, sep='\n', comment=None)).reshape((-1,))
+
+    elif model == "thesis_bad_examples":
+        ref = "when did martin luther , O.S.A. , who was ordained to the priesthood in 1507 , publish his translation of the new testament ?"
+        h1 = "when did publish his translation of the new testament ?"
+        h2 = "augustinian monk martin luther published a new testament of his own translation in year ?"
+
+        benchmark([h1], [[ref]])
+        benchmark([h2], [[ref]])
+
+        exit()
 
     benchmark(candidates, references)

models/RepeatQ/layers/attention.py
@@ -44,7 +44,7 @@ class Attention(tf.keras.layers.Layer):
             dense_input = self.attention_dropout(attention_input)
             dense_result = self.attention_matrix(dense_input)
             scores = self.attention_vector(tf.math.tanh(dense_result))
-            scores = tf.where(tf.expand_dims(mask, axis=-1), scores, tf.zeros_like(scores, dtype=tf.float32))
+            scores = tf.where(tf.expand_dims(mask, axis=-1), scores, tf.ones_like(scores, dtype=tf.float32) * tf.float32.min)
             if apply_softmax:
                 return tf.math.softmax(scores, axis=-2)
             return scores

models/RepeatQ/model.py
@@ -396,13 +396,13 @@ class RepeatQ(LoggingMixin, tf.keras.models.Model):
         question_copy_prob = origin_probs[..., 1:2]
         facts_copy_prob = origin_probs[..., 2:3]
 
-        def zero_out(x):
-            return tf.where(tf.equal(x, 0), tf.zeros_like(x), x)
+        def mask_padding_logits(tokens, logits):
+            return tf.where(tf.equal(tokens, 0), tf.ones_like(logits) * tf.float32.min, logits)
 
         # Prevents copying padding tokens by zero-ing out the logits where 0 tokens are found
-        base_question_logits = zero_out(base_question_logits)
+        base_question_logits = mask_padding_logits(base_question, base_question_logits)
         # Same for facts logits
-        facts_logits = zero_out(facts_logits)
+        facts_logits = mask_padding_logits(flattened_facts, facts_logits)
         base_question_distribution = RepeatQ.stable_softmax(base_question_logits)
         facts_distribution = RepeatQ.stable_softmax(facts_logits)
         copy_distribution = tf.concat((